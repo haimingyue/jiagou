d0e5793c612df47664113d4fc2c37cb25d8e4eed
simoon <420526391@qq.com>
2021-06-23 16:33:09 +0800

    参数解析

--- simoon-test-lib/lib/index.js
+++ simoon-test-lib/lib/index.js
@@ -1,5 +1,12 @@
 module.exports = {
     sum(a, b) {
         return a + b
+    },
+    init({ option, param }) {
+        console.log('init')
+        console.log('执行init方法', option, param)
     }
 }
+
+// --version的实现
+// command是在args的第三位，如果第三为出现了--开头，将成为一个全局的option
