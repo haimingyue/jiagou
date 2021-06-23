893105ccf49d56a619595a2adbae0cf698e05d0d
simoon <420526391@qq.com>
2021-06-23 16:32:02 +0800

    参数解析

--- simoon-test/bin/index.js
+++ simoon-test/bin/index.js
@@ -1,7 +1,24 @@
-#!/usr/bin/env node
-
-console.log('welcome imooc111222')
-
 let lib = require('simoon-test-lib');
-
-console.log(lib.sum(1, 2));
+let argvs = require('process').argv;
+console.log(argvs)
+let command = argvs[2];
+const options = argvs.splice(3);
+if (options.length > 1) {
+    let [option, param] = options;
+    option = option.replace('--', '');
+    if (command) {
+        if (lib[command]) {
+            lib[command]({ option, param })
+        } else {
+            console.log('无效的命令')
+        }
+    } else {
+        console.log('请输入命令')
+    }
+}
+if (command.startsWith('--') || command.startsWith('-')) {
+    const globalOption = command.replace(/--|-/g, '')
+    if (globalOption === 'version' || globalOption === 'V') {
+        console.log('当前版本号是：v1.0.0')
+    }
+}
